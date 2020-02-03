f77c789691ea136d2db9e5dab155a005b8fc62d7
linuxuser <priyadharshini18996>
2020-02-03 14:29:18 +0530

    changes

--- sumsintriangle.py
+++ sumsintriangle.py
@@ -4,14 +4,24 @@ no=int(input('Enter no of rows and columns : '))
 for i in range(no):
   
    lst1=list(map(int,input().split()))[:i+1]
-    
+   
    lst.append(lst1)
+   
 
-for row in range(no-1,-1,-1):
-     for column in range(row):
-             if lst[row][column]>lst[row][column+1]:
-                lst[row-1][column]+=lst[row][column]
-             else:
-                lst[row-1][column]+=lst[row][column+1]
+if no > 1: 
+        lst[1][1] = lst[1][1]+lst[0][0] 
+        lst[1][0] = lst[1][0]+lst[0][0] 
+# Traverse remaining rows 
+for row in range(2, no): 
+        lst[row][0] = lst[row][0] + lst[row-1][0] 
+        lst[row][row] = lst[row][row] + lst[row-1][row-1] 
 
-print("sums of triangle is =", (lst[0][0]))
+    # Loop to traverse columns 
+        for column in range(2,row): 
+           
+            if lst[row][column]+lst[row-1][column-1] >= lst[row][column]+lst[row-1][column]: 
+                lst[row][column] = lst[row][column] + lst[row-1][column-1] 
+            else: 
+                lst[row][column] = lst[row][column]+lst[row-1][column] 
+
+print("sum in triangle is:",(max(lst[no-1])))
